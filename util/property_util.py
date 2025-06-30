0b8a0aafc93fe9df3c3a52ce1e8f2c48789f1a62
Sathiyapriyam13 <sathiyapriyamurugan13@gmail.com>
2025-06-30 23:48:41 +0530

    Add files via upload

---
+++ util/property_util.py
@@ -0,0 +1,11 @@
+# DB property loader
+class DBPropertyUtil:
+
+    @staticmethod
+    def get_db_properties():
+        return {
+            "host": "localhost",
+            "user": "root",
+            "password": "Poojashree",
+            "database": "cars"
+        }
